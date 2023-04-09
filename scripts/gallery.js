6de11e4a86ea95351333da5d23a3843652bfe7fb
Alberto Beltran <albertobeltran@protonmail.com>
2023-04-08 20:39:59 -0600

    created animations for gallery page

---
+++ scripts/gallery.js
@@ -0,0 +1,27 @@
+const images = document.getElementsByTagName('img');
+imgArray = Array.from(images);
+
+let options = {
+    rootMargin: '0px',
+    threshold: .25
+}
+
+let setItemActive = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+
+        if (!(entry.isIntersecting)
+            && entry.target.classList == 'gallery-img active'
+            && navigator.userAgent.toLowerCase().match(/mobile/i)) {
+            entry.target.classList.remove('active');
+        }
+    })
+}
+
+let observer = new IntersectionObserver(setItemActive, options);
+
+imgArray.forEach(img => {
+    observer.observe(img);
+});
